a576286fe1d7c5a675f290a2698e43c15423f618
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-06-26 22:00:48 +0600

    77-6 Styling In NextJS And CSS Modules

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,12 +1,13 @@
 import Meals from "@/components/Meals";
 import React from "react";
-
+import styles from "./styles.module.css";
 const MealPage = async () => {
   // const [search, setSearch] = useState(""); //server page এ useState কাজ করবে না।
 
   return (
     <div className="p-12">
-      <h2>Chose your Meals by search</h2>
+      <h2 className={styles.text_tomato}>Chose your Meals by search</h2>
+      <p className={styles.multiple_style}>Search now for a meal</p>
       {/* meal কে আলাদা একটা component এ রাখার হয়েয়ে কারণ সার্ভার পেজ useState support করে না। */}
       <Meals />
     </div>
