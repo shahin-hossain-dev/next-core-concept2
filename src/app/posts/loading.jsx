dbaddf643850eb7a18e161c3ace6a8826f274b62
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-06-29 11:43:35 +0600

    78-4 Loader With API Data

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+//loading er jonno shudhu route & folder er under e akta loading.jsx file make korlei hoy.
+const loading = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h2>Post data Loading....</h2>
+    </div>
+  );
+};
+
+export default loading;
