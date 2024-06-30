0801fa1a3e538c68b7501335c920065ca582a330
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-06-30 22:36:48 +0600

    79-3 Get NextAuth Session Data In Client And Server Components

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
 // [..nextauth] এটাকে বলে catch all route segments/ daynamic route gulo aikhane ashbe
 const handler = NextAuth({
   //session টা হলো কোন method এ authenticate করবে সেটার session
@@ -7,10 +8,57 @@ const handler = NextAuth({
   },
   providers: [
     //provider declare & settings
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          required: true,
+          placeholder: "enter your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          required: true,
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials; // credentials er moddhe email password pawa jabe
+        if (!credentials) {
+          return null;
+        }
+
+        if (email) {
+          const currentUser = users.find((user) => user.email === email);
+          if (currentUser) {
+            if (currentUser.password === password) {
+              return currentUser;
+            }
+          }
+        }
+        return null;
+      },
+    }),
   ],
   /* pages: {
     //login, signup page gulor directory aikhane set kora jete pare
   }, */
 });
+
+const users = [
+  {
+    email: "s@gmail.com",
+    password: "123456789",
+  },
+  {
+    email: "j@gmail.com",
+    password: "123456789",
+  },
+  {
+    email: "m@gmail.com",
+    password: "123456789",
+  },
+];
 
 export { handler as GET, handler as POST };
